3e4c993434c124271bce0c212a87182531c848b7
featherbear <andrew.j.wong@outlook.com>
2018-12-26 20:58:49 +1100

    HTTP MITM Demo

---
+++ http_mitm/README.md
@@ -0,0 +1,32 @@
+# HTTP(S) / MITM Demo
+---
+
+> This demo explores the importance of HTTPS when dealing with confidential data in website.  
+
+## Teaching Material
+### Overview
+Students are shown the lack of security over HTTP sites, by being able to see their credentials transmitted over a network in plain-text.  
+
+User accounts are able to be compromised even if credentials are encrpyted (ie MD5).  
+
+Performing the same tasks over HTTPS yields a change in results, where packet analysis no longer reveals the password
+
+### Tools
+* Server computer (w/ nodejs)
+* Client computer
+* [Wireshark](https://www.wireshark.org) / [Fiddler](https://www.telerik.com/fiddler) / network packet analyser
+
+---
+## Setup
+### Generate SSL Certificate
+```bash
+cd ssl
+openssl req -nodes -new -x509 -keyout server.key -out server.cert
+```
+
+### Install NodeJS Modules
+`npm install`
+
+---
+## Run
+`node server.js`
---
+++ http_mitm/server.js
@@ -0,0 +1,67 @@
+const superSecretCredentials = {
+  admin: 'FLAG{c4nt_t0uch_m3}',
+  john: 'citizen',
+  andrew: new Buffer.from("\u00A5\u00AB\u002C\u00C2\u008A\u00DD", "\x62\x69\x6e\x61\x72\x79").toString("\x62\x61\x73\x65\x36\x34"),
+  alicia: 'piano'
+}
+
+const md5 = require('md5');
+const superSecretCredentials_hashed = {};
+Object.keys(superSecretCredentials).forEach(function(key) {
+    superSecretCredentials_hashed[key] = md5(superSecretCredentials[key]);
+  }
+)
+
+console.log('Super secret logon details are:')
+console.log('-------------------------------')
+Object.keys(superSecretCredentials).forEach(key => console.log(`Username:\t${key}\nPlain Text:\t${superSecretCredentials[key]}\nPassword Hash:\t${superSecretCredentials_hashed[key]}\n`))
+console.log('-------------------------------')
+
+
+const fs = require('fs');
+const express = require('express');
+const app = express();
+var cors = require('cors');
+const port = 8000;
+const secure_port = 8443;
+
+app.use(express.static('site'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+function sendOK(res) {
+  res.send({status: true})
+}
+function sendNO(res) {
+  res.send({status: false})
+}
+
+app.post('/login_plain', (req, res) => {
+  let username = req.body.username, password = req.body.password;
+  
+  let result = (username && password && superSecretCredentials[username] == password);
+  console.log(`${req.protocol} - /login_plain - ${username||"(blank)"}:${password||"(blank)"} - ${result ? "SUCCESS" : "FAILURE"}`);
+  result ? sendOK(res) : sendNO(res);
+});
+
+app.post('/login_clientHash', (req, res) => {
+  let username = req.body.username, passwordHash = req.body.password;
+  
+  let result = (username && passwordHash && superSecretCredentials_hashed[username] == passwordHash)
+  console.log(`${req.protocol} - /login_clientHash - ${username||"(blank)"}:${passwordHash||"(blank)"} - ${result ? "SUCCESS" : "FAILURE"}`);
+  result ? sendOK(res) : sendNO(res);
+});
+
+
+require('http').createServer(app).listen(port, function() {
+  console.log(`App (HTTP)  listening on port ${port}!`);
+  
+});
+
+require('https').createServer({
+  key: fs.readFileSync('./ssl/server.key'),
+  cert: fs.readFileSync('./ssl/server.cert')
+}, app).listen(8443, function(){
+  console.log(`App (HTTPS) listening on port ${secure_port }!`);
+});
